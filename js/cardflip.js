4cb234bc8cbf77229e9ca6a1e5e09301ae681f0c
theosaurusrex <theosaurus@gmail.com>
2018-12-13 14:29:41 -0600

    random card selected

---
+++ js/cardflip.js
@@ -0,0 +1,15 @@
+var card = document.querySelector('.card');
+card.addEventListener( 'click', function() {
+  card.classList.toggle('is-flipped');
+});
+//card 2
+var card2 = document.querySelector('.card2');
+card2.addEventListener( 'click', function() {
+  card2.classList.toggle('is-flipped');
+});
+//third card
+var card3 = document.querySelector('.card3');
+card3.addEventListener( 'click', function() {
+  card3.classList.toggle('is-flipped');
+});
+//randomly select tarot image
