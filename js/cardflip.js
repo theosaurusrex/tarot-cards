eeb368591804b609aa533ba118915c870d403909
theodore howard <theodorehoward@theodores-MacBook-Air.local>
2019-01-04 18:20:59 -0600

    Fixed iOS click touch problem

--- js/cardflip.js
+++ js/cardflip.js
@@ -3,6 +3,10 @@ var cardOneFlipped = false;
 var cardTwoFlipped = false;
 var cardThreeFlipped = false;
 
+var card1 = document.querySelector('.card1');
+var card2 = document.querySelector('.card2');
+var card3 = document.querySelector('.card3');
+
 var card = document.querySelector('.card');
 
 //document.getElementById("drawThree").disabled = true;
@@ -24,28 +28,56 @@ function checkCards (){
 
 //clicks behavior on cards
 
-card.addEventListener( 'click', function() {
-  card.classList.toggle('is-flipped');
+
+// card.addEventListener( 'click', function() {
+//   card.classList.toggle('is-flipped');
+//   cardOneFlipped = !cardOneFlipped;
+//   console.log("cardOneFlipped is " + cardOneFlipped);
+//   checkCards();  
+// });
+
+//card1 update
+
+card1.addEventListener( 'click', cardOneClicked);
+card1.addEventListener( 'touchstart', cardOneClicked);
+
+function cardOneClicked() {
+  card1.classList.toggle('is-flipped');
   cardOneFlipped = !cardOneFlipped;
   console.log("cardOneFlipped is " + cardOneFlipped);
   checkCards();  
-});
+}
 
 //card 2
-var card2 = document.querySelector('.card2');
-card2.addEventListener( 'click', function() {
+
+card2.addEventListener( 'click', cardTwoClicked);
+card2.addEventListener( 'touchstart', cardTwoClicked);
+
+function cardTwoClicked() {
   card2.classList.toggle('is-flipped');
   cardTwoFlipped = !cardTwoFlipped;
   console.log("cardTwoFlipped " + cardTwoFlipped);
   checkCards();
-});
+}
+
+
+// var card2 = document.querySelector('.card2');
+// card2.addEventListener( 'click', function() {
+//   card2.classList.toggle('is-flipped');
+//   cardTwoFlipped = !cardTwoFlipped;
+//   console.log("cardTwoFlipped " + cardTwoFlipped);
+//   checkCards();
+// });
 
 //third card
-var card3 = document.querySelector('.card3');
-card3.addEventListener( 'click', function() {
+
+card3.addEventListener( 'click', cardThreeClicked);
+card3.addEventListener( 'touchstart', cardThreeClicked);
+
+function cardThreeClicked() {
   card3.classList.toggle('is-flipped');
     cardThreeFlipped = !cardThreeFlipped;
     console.log("cardThreeFlipped " + cardThreeFlipped);
     checkCards();
-});
+}
 //Initially disable btn to drawThree
